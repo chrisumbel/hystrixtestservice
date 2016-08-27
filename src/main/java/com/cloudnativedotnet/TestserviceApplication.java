93f05608dbe13913c377101e7d258d2bf3e8f4a7
Chris Umbel <chris@chrisumbel.com>
2016-08-27 18:10:07 -0400

    enabled health endpoint

--- src/main/java/com/cloudnativedotnet/TestserviceApplication.java
+++ src/main/java/com/cloudnativedotnet/TestserviceApplication.java
@@ -2,8 +2,10 @@ package com.cloudnativedotnet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class TestserviceApplication {
 
 	public static void main(String[] args) {
