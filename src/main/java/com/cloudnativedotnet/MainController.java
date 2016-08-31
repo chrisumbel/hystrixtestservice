18ad26a70e9a8926409df920ba500b8ec05e252b
Chris Umbel <chris@chrisumbel.com>
2016-08-31 09:26:54 -0400

    idomatic

--- src/main/java/com/cloudnativedotnet/MainController.java
+++ src/main/java/com/cloudnativedotnet/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MainController {		
+public class MainController {
 	@Autowired
 	private HipsterIpsumService hipsterIpsumService;
 	
